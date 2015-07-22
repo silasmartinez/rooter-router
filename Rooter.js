c4ffe586a897b9f684d860f11eca9bedf2c6f67d
silasmartinez <silasm@gmail.com>
2015-07-22 08:57:10 -0600

    Moved to semi-standard linting, various code cleanups.

--- Rooter.js
+++ Rooter.js
@@ -1,148 +1,147 @@
-var url = require('url')
+var url = require('url');
 
 var Rooter = function () {
-  this.routeHandlers = {}
-}
+  this.routeHandlers = {};
+};
 
 Rooter.prototype.add = function (routeUrl, routeResponse, routeMethod) {
   if (routeMethod) {
     if (this.routeHandlers.hasOwnProperty(routeUrl)) {
-      throw new Error('Cannot assign a method specific route if a general route exists')
+      throw new Error('Cannot assign a method specific route if a general route exists');
     }
-    var baseRoute = routeUrl
-    routeUrl = routeMethod + routeUrl
-    this.routeHandlers[routeUrl] = {}
-    this.routeHandlers[routeUrl].method = routeMethod
-    this.routeHandlers[routeUrl].baseRoute = baseRoute
+    var baseRoute = routeUrl;
+    routeUrl = routeMethod + routeUrl;
+    this.routeHandlers[routeUrl] = {};
+    this.routeHandlers[routeUrl].method = routeMethod;
+    this.routeHandlers[routeUrl].baseRoute = baseRoute;
   } else {
-    for (route in this.routeHandlers) {
+    for (var route in this.routeHandlers) {
       if (this.routeHandlers[route].baseRoute === routeUrl) {
-        throw new Error('Cannot assign a general route if a method specific route exists')
+        throw new Error('Cannot assign a general route if a method specific route exists');
       }
     }
-    this.routeHandlers[routeUrl] = {}
+    this.routeHandlers[routeUrl] = {};
   }
-  this.routeHandlers[routeUrl].func = routeResponse
-  var dynamicCheck = []
-  var isSplat
+  this.routeHandlers[routeUrl].func = routeResponse;
+  var dynamicCheck = [];
+  var isSplat;
   routeUrl.split('/').forEach(function (member, index) {
     if (member[0] === ':') {
       if (dynamicCheck.indexOf(member) < 0) {
-        dynamicCheck.push(member)
+        dynamicCheck.push(member);
       } else {
-        throw new Error('route {' + routeUrl + '} contains duplicate dynamic segment {' + member + '}')
+        throw new Error('route {' + routeUrl + '} contains duplicate dynamic segment {' + member + '}');
       }
     } else if (member[member.length - 1] === '*') {
-      isSplat = true
+      isSplat = true;
     }
-  })
-  if (isSplat) this.routeHandlers[routeUrl].isSplat = true
-  this.routeHandlers[routeUrl].hasLength = routeUrl.split('/').length
-  return this
-}
+  });
+  if (isSplat) this.routeHandlers[routeUrl].isSplat = true;
+  this.routeHandlers[routeUrl].hasLength = routeUrl.split('/').length;
+  return this;
+};
 
 Rooter.prototype._getBestMatch = function (reqUrlArray, reqMethod) {
-  var matches = []
-  for (route in this.routeHandlers) {
+  var matches = [];
+  for (var route in this.routeHandlers) {
     var matchObj = {
       'namedRoute': route,
       'matchRate': reqUrlArray.length
-    }
-    if (reqUrlArray.length === parseInt(this.routeHandlers[route].hasLength) && ( this.routeHandlers[route].method === reqMethod || !this.routeHandlers[route].method)) {
-
-      var routeArray = route.split('/')
-      var isMatch = true
+    };
+    if (reqUrlArray.length === parseInt(this.routeHandlers[route].hasLength, 10) && (this.routeHandlers[route].method === reqMethod || !this.routeHandlers[route].method)) {
+      var routeArray = route.split('/');
+      var isMatch = true;
       reqUrlArray.forEach(function (ele, ind) {
         if (ind > 0) {
           if (routeArray[ind][0] === ':') {
-            matchObj.matchRate += 1
+            matchObj.matchRate += 1;
           } else if (ele === routeArray[ind]) {
-            matchObj.matchRate += 2
+            matchObj.matchRate += 2;
           } else {
-            isMatch = false
+            isMatch = false;
           }
         }
-      })
-      isMatch && matches.push(matchObj)
+      });
+      isMatch && matches.push(matchObj);
     }
   }
   if (matches.length > 0) {
     return matches.reduce(function (a, cur) {
-      return a.matchRate > cur.matchRate ? a : cur
-    })
+      return a.matchRate > cur.matchRate ? a : cur;
+    });
   } else {
-    var splatResults = this._checkSplats(reqUrlArray)
+    var splatResults = this._checkSplats(reqUrlArray);
     if (splatResults) {
-      return splatResults
+      return splatResults;
     } else {
-      return false
+      return false;
     }
   }
-}
+};
 
 Rooter.prototype._checkSplats = function (reqUrlArray) {
-  var splatMatchArr = []
-  for (splat in this.routeHandlers) {
-    var matchSplat = true,
-      splatArr = splat.split('/')
+  var splatMatchArr = [];
+  for (var splat in this.routeHandlers) {
+    var matchSplat = true;
+    var splatArr = splat.split('/');
     if (this.routeHandlers[splat].isSplat) {
       for (var i = 1; i < splatArr.length - 1; i++) {
         if (splatArr[i] !== reqUrlArray[i]) {
-          matchSplat = false
+          matchSplat = false;
         }
       }
       if (matchSplat) {
-        splatMatchArr.push( { 'namedRoute' : splat,
-                'specificity' : splatArr.length} )
+        splatMatchArr.push({
+          'namedRoute': splat,
+          'specificity': splatArr.length
+        });
       }
     }
   }
   if (splatMatchArr.length > 0) {
     return splatMatchArr.reduce(function (a, cur) {
-      return a.specificity > cur.specificity ? a : cur
-    })
+      return a.specificity > cur.specificity ? a : cur;
+    });
   } else {
-    return false
+    return false;
   }
-}
+};
 
 Rooter.prototype.handle = function (req, res) {
-  var path = url.parse(req.url).pathname
+  var path = url.parse(req.url).pathname;
   if (path[path.length - 1] === '/' && path !== '/') {
-    path = path.slice(0, -1)
+    path = path.slice(0, -1);
   }
-  var reqUrlArray = path.split('/')
-  var chosenRoute = this._getBestMatch(reqUrlArray, req.method).namedRoute
+  var reqUrlArray = path.split('/');
+  var chosenRoute = this._getBestMatch(reqUrlArray, req.method).namedRoute;
   if (chosenRoute) {
-    var helper = url.parse(req.url)
-    helper.verb = req.method
-    if (! this.routeHandlers[chosenRoute].isSplat) {
-      helper.resources = []
-      helper.dynamics = {}
-      var routeArray = chosenRoute.split('/')
+    var helper = url.parse(req.url);
+    helper.verb = req.method;
+    if (!this.routeHandlers[chosenRoute].isSplat) {
+      helper.resources = [];
+      helper.dynamics = {};
+      var routeArray = chosenRoute.split('/');
       reqUrlArray.forEach(function (ele, ind) {
         if (ind > 0) {
           if (routeArray[ind][0] === ':') {
-            helper.dynamics[routeArray[ind].substring(1)] = ele
+            helper.dynamics[routeArray[ind].substring(1)] = ele;
           } else if (ele === routeArray[ind]) {
-            helper.resources.push(ele)
-          } else {
-            isMatch = false
+            helper.resources.push(ele);
           }
         }
-      })
+      });
     } else {
-      helper.isSplat = true
+      helper.isSplat = true;
     }
-    helper.routeMatched = chosenRoute
-    this.routeHandlers[chosenRoute].func(req, res, helper)
-    return
+    helper.routeMatched = chosenRoute;
+    this.routeHandlers[chosenRoute].func(req, res, helper);
+    return;
   } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'})
-    res.write('404: Resource not found\n')
-    res.write(path + ' (' + req.method + ')')
-    res.end()
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write('404: Resource not found\n');
+    res.write(path + ' (' + req.method + ')');
+    res.end();
   }
-}
+};
 
-module.exports = Rooter
+module.exports = Rooter;
--- spec/rooterSpec.js
+++ spec/rooterSpec.js
@@ -1,148 +1,147 @@
-var Rooter = require('../Rooter')
-var http = require('http')
-var request = require('supertest')
+var Rooter = require('../Rooter');
+var http = require('http');
+var request = require('supertest');
 
-var testRouter = new Rooter
+var testRouter = new Rooter;
 
 var testServer = http.createServer(function (req, res) {
-  testRouter.handle(req, res)
-})
+  testRouter.handle(req, res);
+});
 
-var app = testServer
-
+var app = testServer;
 
 describe('#add()', function () {
   it('should add a route to routeHandlers', function () {
-    testRouter.add('/things', function (req, res) {res.end('success')})
-    expect( testRouter.routeHandlers['/things'].func.toString() )
-      .toEqual(function (req, res) {res.end('success')}.toString())
-  })
-  it("should throw when trying to add a method specific route when a general exists", function () {
+    testRouter.add('/things', function (req, res) {res.end('success');});
+    expect(testRouter.routeHandlers['/things'].func.toString())
+      .toEqual(function (req, res) {res.end('success');}.toString());
+  });
+  it('should throw when trying to add a method specific route when a general exists', function () {
     expect(function () {
-      testRouter.add('/things', function () {},'POST')
+      testRouter.add('/things', function () {}, 'POST');
     }).toThrow(
       new Error('Cannot assign a method specific route if a general route exists')
-    )
+    );
   });
-})
+});
 
-describe("#handle()", function () {
-  it("should 404 for an unknown route", function (done) {
+describe('#handle()', function () {
+  it('should 404 for an unknown route', function (done) {
     request(app)
       .get('/thisroutedoesnotexist')
       .expect(404)
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) {
-          done.fail(err)
+          done.fail(err);
         } else {
-          done()
+          done();
         }
-      })
+      });
   });
-  it("should handle a defined route", function (done) {
+  it('should handle a defined route', function (done) {
     request(app)
       .get('/things')
       .expect('success')
       .expect(200)
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) {
-          done.fail(err)
+          done.fail(err);
         } else {
-          done()
+          done();
         }
-      })
+      });
   });
-  it("should generate a helper object", function (done) {
+  it('should generate a helper object', function (done) {
     testRouter.add('/helptest', function (req, res, helper) {
-      res.end(helper.resources[0])
-    })
+      res.end(helper.resources[0]);
+    });
     request(app)
       .get('/helptest')
       .expect('helptest')
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) {
-          done.fail(err)
+          done.fail(err);
         } else {
-          done()
+          done();
         }
-      })
+      });
   });
-  it("should prefer a more specific route", function (done) {
+  it('should prefer a more specific route', function (done) {
     testRouter.add('/foo/:bar', function (req, res, helper) {
-      res.end(helper.routeMatched)
+      res.end(helper.routeMatched);
     })
       .add('/foo/baz', function (req, res, helper) {
-      res.end(helper.routeMatched)
-    })
+        res.end(helper.routeMatched);
+      });
     request(app)
       .get('/foo/baz')
       .expect('/foo/baz')
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) {
-          done.fail(err)
+          done.fail(err);
         } else {
-          done()
+          done();
         }
-      })
+      });
   });
-  it("should handle multiple dynamic segments", function (done) {
+  it('should handle multiple dynamic segments', function (done) {
     testRouter.add('/foo/:bar/baz/:fa', function (req, res, helper) {
-      res.end(helper.dynamics.bar + helper.dynamics.fa)
-    })
+      res.end(helper.dynamics.bar + helper.dynamics.fa);
+    });
     request(app)
       .get('/foo/stuff/baz/nthings')
       .expect('stuffnthings')
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) {
-          done.fail(err)
+          done.fail(err);
         } else {
-          done()
+          done();
         }
-      })
+      });
   });
-  it("should handle splats", function (done) {
+  it('should handle splats', function (done) {
     testRouter.add('/public/*', function (req, res, helper) {
-      res.end(helper.routeMatched)
-    })
+      res.end(helper.routeMatched);
+    });
     request(app)
       .get('/public/styles/test.css')
       .expect('/public/*')
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) {
-          done.fail(err)
+          done.fail(err);
         } else {
-          done()
+          done();
         }
-      })
+      });
   });
-  it("should not treat splats as most specific", function (done) {
+  it('should not treat splats as most specific', function (done) {
     testRouter.add('/public/resource/:test', function (req, res, helper) {
-      res.end(helper.dynamics.test)
-    })
+      res.end(helper.dynamics.test);
+    });
     request(app)
       .get('/public/resource/foobar')
       .expect('foobar')
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) {
-          done.fail(err)
+          done.fail(err);
         } else {
-          done()
+          done();
         }
-      })
+      });
   });
-  it("should still prefer the most specific splat", function (done) {
+  it('should still prefer the most specific splat', function (done) {
     testRouter.add('/public/special/*', function (req, res, helper) {
-      res.end(helper.routeMatched)
-    })
+      res.end(helper.routeMatched);
+    });
     request(app)
       .get('/public/special/foo/bar/baz')
       .expect('/public/special/*')
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) {
-          done.fail(err)
+          done.fail(err);
         } else {
-          done()
+          done();
         }
-      })
+      });
   });
 });
